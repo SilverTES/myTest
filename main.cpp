b49ef5e99c2a0054e8b3e85b36a00f3a36ffc96f
silvertes <silvertes.pro@gmail.com>
2015-02-15 20:05:25 +0100

    Rajout de 'Hi'

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 
 {
-    cout << " Master : SilverTES " << endl ;
+    cout << " Hi Master : SilverTES " << endl ;
     cout << "--- ©2015 MugenStudio ---" << endl ;
     cout << "Ma Nouvelle fonctionnalité" << endl ;
     cout << "Encore un truc trop bien :)!" << endl ;
