62633068b7ad4aa9ed33b25871ec7a4a5ce9ce7f
silvertes <silvertes.pro@gmail.com>
2015-02-15 18:48:34 +0100

    add another func

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     cout << "--- ©2015 MugenStudio ---" << endl ;
     cout << "Ma Nouvelle fonctionnalité" << endl ;
-
+    cout << "Encore un truc trop bien :)!" << endl ;
     return 0;
 
 }
